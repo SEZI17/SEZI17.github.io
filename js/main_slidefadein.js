d31e31f3c383be54bc27c41e744898db77d0388e
SEZI <sae0817@naver.com>
2019-09-12 16:34:29 +0900

    메인 구성
    index.html

---
+++ js/main_slidefadein.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('#MAI_content1_inf').each(function(i){            
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1','margin-left':'-300px'},3000);
+            }
+            
+        }); 
+    });
+});
