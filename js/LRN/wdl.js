7b3c1271759d20b79cb4f87e52d88247b46e8916
SEZI <sae0817@naver.com>
2019-09-17 01:22:41 +0900

    단어 list 페이지 (대체)
    wdl.html 스타트업

---
+++ js/LRN/wdl.js
@@ -0,0 +1,238 @@
+function wordlistoutput_a() {
+    for(let i=0; i<form_a.length; i++) {
+    var form = form_a[i];
+    var test = test_a[i];   
+    var kanji = kanji_a[i];
+    var yomigana = yomigana_a[i];
+    var korean = korean_a[i];
+    var WDLlist = document.getElementById('LRN_WDL_table_list_a');
+    var add = WDLlist.insertRow( WDLlist.rows.length );
+    var WDL_formcell = add.insertCell(0);
+    var WDL_testcell = add.insertCell(1);
+    var WDL_kanjicell = add.insertCell(2);
+    var WDL_yomiganacell = add.insertCell(3);
+    var WDL_koreancell = add.insertCell(4);
+    WDL_formcell.innerHTML = form;
+    WDL_testcell.innerHTML = test;
+    WDL_kanjicell.innerHTML = kanji;
+    WDL_yomiganacell.innerHTML = yomigana;
+    WDL_koreancell.innerHTML = korean;
+    }
+}
+
+function wordlistoutput_ka() {
+    for(let i=0; i<form_ka.length; i++) {
+    var form = form_ka[i];
+    var test = test_ka[i];   
+    var kanji = kanji_ka[i];
+    var yomigana = yomigana_ka[i];
+    var korean = korean_ka[i];
+    var WDLlist = document.getElementById('LRN_WDL_table_list_ka');
+    var add = WDLlist.insertRow( WDLlist.rows.length );
+    var WDL_formcell = add.insertCell(0);
+    var WDL_testcell = add.insertCell(1);
+    var WDL_kanjicell = add.insertCell(2);
+    var WDL_yomiganacell = add.insertCell(3);
+    var WDL_koreancell = add.insertCell(4);
+    WDL_formcell.innerHTML = form;
+    WDL_testcell.innerHTML = test;
+    WDL_kanjicell.innerHTML = kanji;
+    WDL_yomiganacell.innerHTML = yomigana;
+    WDL_koreancell.innerHTML = korean;
+    }
+}
+
+function wordlistoutput_sa() {
+    for(let i=0; i<form_sa.length; i++) {
+    var form = form_sa[i];
+    var test = test_sa[i];   
+    var kanji = kanji_sa[i];
+    var yomigana = yomigana_sa[i];
+    var korean = korean_sa[i];
+    var WDLlist = document.getElementById('LRN_WDL_table_list_sa');
+    var add = WDLlist.insertRow( WDLlist.rows.length );
+    var WDL_formcell = add.insertCell(0);
+    var WDL_testcell = add.insertCell(1);
+    var WDL_kanjicell = add.insertCell(2);
+    var WDL_yomiganacell = add.insertCell(3);
+    var WDL_koreancell = add.insertCell(4);
+    WDL_formcell.innerHTML = form;
+    WDL_testcell.innerHTML = test;
+    WDL_kanjicell.innerHTML = kanji;
+    WDL_yomiganacell.innerHTML = yomigana;
+    WDL_koreancell.innerHTML = korean;
+    }
+}
+
+function wordlistoutput_ta() {
+    for(let i=0; i<form_ta.length; i++) {
+    var form = form_ta[i];
+    var test = test_ta[i];   
+    var kanji = kanji_ta[i];
+    var yomigana = yomigana_ta[i];
+    var korean = korean_ta[i];
+    var WDLlist = document.getElementById('LRN_WDL_table_list_ta');
+    var add = WDLlist.insertRow( WDLlist.rows.length );
+    var WDL_formcell = add.insertCell(0);
+    var WDL_testcell = add.insertCell(1);
+    var WDL_kanjicell = add.insertCell(2);
+    var WDL_yomiganacell = add.insertCell(3);
+    var WDL_koreancell = add.insertCell(4);
+    WDL_formcell.innerHTML = form;
+    WDL_testcell.innerHTML = test;
+    WDL_kanjicell.innerHTML = kanji;
+    WDL_yomiganacell.innerHTML = yomigana;
+    WDL_koreancell.innerHTML = korean;
+    }
+}
+
+function wordlistoutput_na() {
+    for(let i=0; i<form_na.length; i++) {
+    var form = form_na[i];
+    var test = test_na[i];   
+    var kanji = kanji_na[i];
+    var yomigana = yomigana_na[i];
+    var korean = korean_na[i];
+    var WDLlist = document.getElementById('LRN_WDL_table_list_na');
+    var add = WDLlist.insertRow( WDLlist.rows.length );
+    var WDL_formcell = add.insertCell(0);
+    var WDL_testcell = add.insertCell(1);
+    var WDL_kanjicell = add.insertCell(2);
+    var WDL_yomiganacell = add.insertCell(3);
+    var WDL_koreancell = add.insertCell(4);
+    WDL_formcell.innerHTML = form;
+    WDL_testcell.innerHTML = test;
+    WDL_kanjicell.innerHTML = kanji;
+    WDL_yomiganacell.innerHTML = yomigana;
+    WDL_koreancell.innerHTML = korean;
+    }
+}
+
+function wordlistoutput_ha() {
+    for(let i=0; i<form_ha.length; i++) {
+    var form = form_ha[i];
+    var test = test_ha[i];   
+    var kanji = kanji_ha[i];
+    var yomigana = yomigana_ha[i];
+    var korean = korean_ha[i];
+    var WDLlist = document.getElementById('LRN_WDL_table_list_ha');
+    var add = WDLlist.insertRow( WDLlist.rows.length );
+    var WDL_formcell = add.insertCell(0);
+    var WDL_testcell = add.insertCell(1);
+    var WDL_kanjicell = add.insertCell(2);
+    var WDL_yomiganacell = add.insertCell(3);
+    var WDL_koreancell = add.insertCell(4);
+    WDL_formcell.innerHTML = form;
+    WDL_testcell.innerHTML = test;
+    WDL_kanjicell.innerHTML = kanji;
+    WDL_yomiganacell.innerHTML = yomigana;
+    WDL_koreancell.innerHTML = korean;
+    }
+}
+
+function wordlistoutput_ma() {
+    for(let i=0; i<form_ma.length; i++) {
+    var form = form_ma[i];
+    var test = test_ma[i];   
+    var kanji = kanji_ma[i];
+    var yomigana = yomigana_ma[i];
+    var korean = korean_ma[i];
+    var WDLlist = document.getElementById('LRN_WDL_table_list_ma');
+    var add = WDLlist.insertRow( WDLlist.rows.length );
+    var WDL_formcell = add.insertCell(0);
+    var WDL_testcell = add.insertCell(1);
+    var WDL_kanjicell = add.insertCell(2);
+    var WDL_yomiganacell = add.insertCell(3);
+    var WDL_koreancell = add.insertCell(4);
+    WDL_formcell.innerHTML = form;
+    WDL_testcell.innerHTML = test;
+    WDL_kanjicell.innerHTML = kanji;
+    WDL_yomiganacell.innerHTML = yomigana;
+    WDL_koreancell.innerHTML = korean;
+    }
+}
+
+function wordlistoutput_ya() {
+    for(let i=0; i<form_ya.length; i++) {
+    var form = form_ya[i];
+    var test = test_ya[i];   
+    var kanji = kanji_ya[i];
+    var yomigana = yomigana_ya[i];
+    var korean = korean_ya[i];
+    var WDLlist = document.getElementById('LRN_WDL_table_list_ya');
+    var add = WDLlist.insertRow( WDLlist.rows.length );
+    var WDL_formcell = add.insertCell(0);
+    var WDL_testcell = add.insertCell(1);
+    var WDL_kanjicell = add.insertCell(2);
+    var WDL_yomiganacell = add.insertCell(3);
+    var WDL_koreancell = add.insertCell(4);
+    WDL_formcell.innerHTML = form;
+    WDL_testcell.innerHTML = test;
+    WDL_kanjicell.innerHTML = kanji;
+    WDL_yomiganacell.innerHTML = yomigana;
+    WDL_koreancell.innerHTML = korean;
+    }
+}
+
+function wordlistoutput_ra() {
+    for(let i=0; i<form_ra.length; i++) {
+    var form = form_ra[i];
+    var test = test_ra[i];   
+    var kanji = kanji_ra[i];
+    var yomigana = yomigana_ra[i];
+    var korean = korean_ra[i];
+    var WDLlist = document.getElementById('LRN_WDL_table_list_ra');
+    var add = WDLlist.insertRow( WDLlist.rows.length );
+    var WDL_formcell = add.insertCell(0);
+    var WDL_testcell = add.insertCell(1);
+    var WDL_kanjicell = add.insertCell(2);
+    var WDL_yomiganacell = add.insertCell(3);
+    var WDL_koreancell = add.insertCell(4);
+    WDL_formcell.innerHTML = form;
+    WDL_testcell.innerHTML = test;
+    WDL_kanjicell.innerHTML = kanji;
+    WDL_yomiganacell.innerHTML = yomigana;
+    WDL_koreancell.innerHTML = korean;
+    }
+}
+
+function wordlistoutput_wa() {
+    for(let i=0; i<form_wa.length; i++) {
+    var form = form_wa[i];
+    var test = test_wa[i];   
+    var kanji = kanji_wa[i];
+    var yomigana = yomigana_wa[i];
+    var korean = korean_wa[i];
+    var WDLlist = document.getElementById('LRN_WDL_table_list_wa');
+    var add = WDLlist.insertRow( WDLlist.rows.length );
+    var WDL_formcell = add.insertCell(0);
+    var WDL_testcell = add.insertCell(1);
+    var WDL_kanjicell = add.insertCell(2);
+    var WDL_yomiganacell = add.insertCell(3);
+    var WDL_koreancell = add.insertCell(4);
+    WDL_formcell.innerHTML = form;
+    WDL_testcell.innerHTML = test;
+    WDL_kanjicell.innerHTML = kanji;
+    WDL_yomiganacell.innerHTML = yomigana;
+    WDL_koreancell.innerHTML = korean;
+    }
+}
+
+$(function(){
+    $('.WDL_col_title').click(this, function(){
+        $(this).next(".WDL_col_table_wrap").stop().slideToggle();
+    });
+});
+
+$(document).ready(function(){
+    wordlistoutput_a();
+    wordlistoutput_ka();
+    wordlistoutput_sa();
+    wordlistoutput_ta();
+    wordlistoutput_na();
+    wordlistoutput_ha();
+    wordlistoutput_ma();
+    wordlistoutput_ya();
+    wordlistoutput_ra();
+    wordlistoutput_wa();
+});
