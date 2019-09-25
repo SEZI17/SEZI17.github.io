3aaf4f6d2bd5ded136faae49fcde831b84b26433
Jang Sejin <54394541+SEZI17@users.noreply.github.com>
2019-09-25 21:13:02 +0900

    메인 모바일페이지 스타트업
    메인 모바일페이지

--- js/main_slidefadein.js
+++ js/main_slidefadein.js
@@ -1,13 +1,18 @@
-$(document).ready(function() {
-    $(window).scroll( function(){
+function slidefadein() {
+    $(window).scroll(function(){
         $('#MAI_content1_inf').each(function(i){            
             var bottom_of_element = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             if( bottom_of_window > bottom_of_element ){
                 $(this).animate({'opacity':'1','margin-left':'-300px'},3000);
-            }
-            
-        }); 
-    });
+            }            
+        });
+    }); 
+}
+
+$(document).ready(function(){
+    if($(window).width() > 361) {
+        slidefadein();
+    }
 });
