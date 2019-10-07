7e8fcea47dbe33eeee460c205ff092d35710ec52
Hyeonwoo Jeong <hyeonwoojeong@Hyeonwooui-Macmini.local>
2019-10-08 03:41:07 +0900

    회원정보변경페이지 수정

--- js/user.js
+++ js/user.js
@@ -35,8 +35,8 @@ function isKorean(input) {
         return false;
     }
 }
-function isEmail(input){
-    var pattern=/^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]/;
+function isEmail(input) {
+    var pattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]/;
     if (pattern.test(input)) {
         return true;
     }
@@ -57,7 +57,39 @@ function clearError(id) {
 }
 
 //edit info
-function editInfoCheck() {
+function editInfoConfirm() {
+    //check nickname
+    var nickname = document.getElementById("nicknameInput");
+    var nicknameError = document.getElementById("nicknameInputError");
+    var nicknameValid = !(isEnglish(nickname.value)) && !(isSpecial(nickname.value)) && lengthBetween(nickname.value, 2, 10);
+    if (nickname.value.length == 0) {
+        nicknameError.innerHTML = "닉네임을 입력해 주세요";
+        nickname.focus();
+        return;
+    }
+    if (!nicknameValid) {
+        nicknameError.innerHTML = "닉네임은 2~10글자 한글로 입력해주세요.";
+        nickname.focus();
+        return;
+    }
+    //check email
+    var email = document.getElementById("emailInput");
+    var emailError = document.getElementById("emailInputError");
+    var emailValid = isEmail(email.value);
+    if (email.value.length == 0) {
+        emailError.innerHTML = "이메일을 입력해 주세요";
+        email.focus();
+        return;
+    }
+    if (!emailValid) {
+        emailError.innerHTML = "이메일 형식이 올바르지 않습니다.";
+        email.focus();
+        return;
+    }
+}
+
+//signup button
+function signupConfirm() {
     //check name
     var name = document.getElementById("nameInput");
     var nameError = document.getElementById("nameInputError");
@@ -114,6 +146,35 @@ function editInfoCheck() {
         email.focus();
         return;
     }
+    //check password
+    var password = document.getElementById("passwordInput");
+    var passwordError = document.getElementById("passwordInputError");
+    var passwordValid = lengthBetween(password.value, 8, 20);
+    if (password.value.length == 0) {
+        passwordError.innerHTML = "비밀번호를 입력해 주세요";
+        password.focus();
+        return;
+    }
+    if (!passwordValid) {
+        passwordError.innerHTML = "비밀번호는 8~20자리로 입력해주세요.";
+        password.focus();
+        return;
+    }
+    //check password check
+    var passwordCheck = document.getElementById("passwordCheckInput");
+    var passwordCheckError = document.getElementById("passwordCheckInputError");
+    var passwordCheckValid = (password.value == passwordCheck.value);
+    if (passwordCheck.value.length == 0) {
+        passwordCheckError.innerHTML = "비밀번호 확인을 입력해 주세요";
+        passwordCheck.focus();
+        return;
+    }
+    if (!passwordCheckValid) {
+        passwordCheckError.innerHTML = "비밀번호가 일치하지 않습니다.";
+        passwordCheck.focus();
+        return;
+    }
+    alert("회원가입을 축하합니다.");
 }
 
 //terms button
