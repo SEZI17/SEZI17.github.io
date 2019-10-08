ecbbc06568d20622b197a40ad0c32c86ee3c8fa1
Jang Sejin <54394541+SEZI17@users.noreply.github.com>
2019-10-08 22:36:19 +0900

    소개 버그 수정
    debugging

--- js/INF/phone1.js
+++ js/INF/phone1.js
@@ -25,3 +25,31 @@ $(window).resize(function() {
         });  
     }
 });
+
+$(function() {
+    if($(window).width() > 768) {
+        $('.front').click(function(){
+            $(this).css('display','block');
+            $(this).next().css('display','block');
+        });
+    
+        $('.back').click(function(){
+            $(this).css('display','block');
+            $(this).prev().css('display','block');
+        });  
+    }
+});
+
+$(window).resize(function() {
+    if($(window).width() > 768) {
+        $('.front').click(function(){
+            $(this).css('display','block');
+            $(this).next().css('display','block');
+        });
+    
+        $('.back').click(function(){
+            $(this).css('display','block');
+            $(this).prev().css('display','block');
+        });  
+    }
+});
