e00153d966687bf42f49f70a2e54eb027c14c395
SEZI <sae0817@naver.com>
2019-10-21 02:37:09 +0900

    2차 페이지점검
    및 내 단어장 구현

--- js/MYP/mynote.js
+++ js/MYP/mynote.js
@@ -5,6 +5,7 @@ function Mynotelistoutput() {
     var kanji = kanji_a[i];
     var yomigana = yomigana_a[i];
     var korean = korean_a[i];
+    var checkbox = "<input id='wordChk"+ i +"' class='wordChkbox' type='checkbox' name='delete' value='apply' /><label for='wordChk"+ i +"' />";
     var WDLlist = document.getElementById('MYP_MYN_notelist');
     var add = WDLlist.insertRow( WDLlist.rows.length );
     var MYN_formcell = add.insertCell(0);
@@ -17,37 +18,43 @@ function Mynotelistoutput() {
     MYN_kanjicell.innerHTML = kanji;
     MYN_yomiganacell.innerHTML = yomigana;
     MYN_koreancell.innerHTML = korean;
-    MYN_deletecell.innerHTML = "삭제";
+    MYN_deletecell.innerHTML = checkbox;
     MYN_timecell.innerHTML = new Date().toDateString();
-    MYN_deletecell.className = "delete_tr";    
+    MYN_timecell.className = "table_time";
+    add.className = "notelist_tr";  
     }
 }
 function membernameOutput() {
     var memberlist = document.getElementById('member_name');
+    var memberlist_m = document.getElementById('member_name_mobile');
     var name = membername[1];
     memberlist.innerHTML = name;
+    memberlist_m.innerHTML = name;
 }
 
-// 모르는단어 전체삭제
-function dknowlistdelete() {
-    $('#LRN_HRD_dknowlist tr').remove();
+// 단어장 리스트 전체선택
+function checkallWord() {
+    if( $("#all_check").is(':checked') ){
+        $("input[name=delete]").prop("checked", true);
+    } else {
+        $("input[name=delete]").prop("checked", false);
+    }
 }
 
-// 모르는단어 선택삭제
-function delWordKJH(){    
-    $('.delete_tr').unbind();
-    $('.delete_tr').bind("click", function(){
+// 단어장 리스트 선택삭제
+function deleteWord(){
+    if($('#MYP_MYN_notelist input:checked').length == 0){           
+        alert("삭제할 단어가 선택되지 않았습니다.");
+    } else {
         var cfdelete = confirm("해당 단어를 삭제하시겠습니까?")
-        if (cfdelete) {      
-        var listline = $(this).closest('tr');
-        listline.css("background-color","#E7C9A7");
-        listline.fadeOut(400, function(){
-            listline.remove();
-        });
-        return false;
-        }
-        else { return false; }
-    });   
+        if (cfdelete) {
+        $('#MYP_MYN_notelist input:checked').each(function(index){  
+            var checkedWord = $(this).parent().parent();                
+            checkedWord.remove();      
+            });            
+            return false;   
+        } else { return false; } 
+    } 
 }
 
 
